0cc8f8cf5ab769f800b5ef3e737a852fec8ce428
Jeroen Heijmans <github@jeroenheijmans.nl>
2018-12-10 17:48:41 +0100

    Add initial real API integration test

---
+++ SampleAspNetWithEfCore.Tests/Integration/PeopleControllerTests.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+
+namespace SampleAspNetWithEfCore.Tests.Integration
+{
+    public class PeopleControllerTests : IntegrationTests
+    {
+        public PeopleControllerTests(CustomWebApplicationFactory factory) 
+            : base(factory)
+        { }
+
+        [Fact]
+        public async Task Get_returns_success_result()
+        {
+            var client = _factory.CreateClient();
+            var response = await client.GetAsync("/api/people");
+            response.EnsureSuccessStatusCode();
+        }
+    }
+}
