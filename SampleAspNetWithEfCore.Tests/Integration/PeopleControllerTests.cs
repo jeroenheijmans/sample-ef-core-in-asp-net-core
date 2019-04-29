db82121f5f7746f69502fc74ba3aec6de9c82a46
Jeroen Heijmans <github@jeroenheijmans.nl>
2019-04-29 22:23:53 +0200

    Add integration test for POST

--- SampleAspNetWithEfCore.Tests/Integration/PeopleControllerTests.cs
+++ SampleAspNetWithEfCore.Tests/Integration/PeopleControllerTests.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿using SampleAspNetWithEfCore.Entities;
+using System.Net.Http;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace SampleAspNetWithEfCore.Tests.Integration
@@ -15,6 +17,21 @@ namespace SampleAspNetWithEfCore.Tests.Integration
             var client = _factory.CreateClient();
             var response = await client.GetAsync("/api/people");
             response.EnsureSuccessStatusCode();
+        }
+
+        [Fact]
+        public async Task Post_returns_created_item()
+        {
+            var client = _factory.CreateClient();
+            var response = await client.PostAsJsonAsync("/api/people", new
+            {
+                name = "John Doe",
+                pet = new { name = "Miffy", animal = "Cat" }
+            });
+
+            var result = await response.DeserializeContentAs<Person>();
+
+            Assert.Equal("John Doe", result.Name);
         }
     }
 }
